9586e1a4213d7322779218702a10eb5d74ab7579
Kevin Chen <blackkey.destiny@gmail.com>
2018-02-01 18:19:29 +0800

    实例演示:自定义HttpMessageHandler实现HTTP方法重写(s305)

---
+++ S305/WebApi/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "Get";
+        }
+
+        public string Post()
+        {
+            return "Post";
+        }
+
+        public string Put()
+        {
+            return "Put";
+        }
+
+        public string Delete()
+        {
+            return "Delete";
+        }
+    }
+}
---
+++ S305/WebApi/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApi
+{
+public class WebApiApplication : System.Web.HttpApplication
+{
+    protected void Application_Start()
+    {
+        // 1、添加自定义的HttpMessageHandler到消息处理管道
+        GlobalConfiguration.Configuration.MessageHandlers.Add(new HttpMethodOverrideHandler());
+
+        // 2、注册路由
+        AreaRegistration.RegisterAllAreas();
+        GlobalConfiguration.Configure(WebApiConfig.Register);
+    }
+}
+}
